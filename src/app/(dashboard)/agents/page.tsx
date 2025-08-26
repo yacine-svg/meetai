b9d05fbf897a621877501f0c61e3d71bfe1beb8d
yacinesekhri <136460089+yacinesekhri@users.noreply.github.com>
2025-08-26 01:32:14 +0100

    11: agents form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,15 +1,30 @@
 import { LoadingState } from "@/components/loading-state";
+import { auth } from "@/lib/auth";
+import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 import { AgentsView, AgentsViewError } from "@/modules/agents/ui/views/agents-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 const Page = async () => {
+    const session = await auth.api.getSession({
+            headers: await headers(),
+        });
+    
+        if(!session) {
+            redirect("/sign-in");
+        }
+    
+    
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
 
     return (
+        <>
+        <AgentsListHeader />
         <HydrationBoundary state={dehydrate(queryClient)}>
             <Suspense fallback={<LoadingState title="Loading Agents" description="Please wait while we fetch the agents."/>}> 
                 <ErrorBoundary fallback= {<AgentsViewError />}>
@@ -17,6 +32,7 @@ const Page = async () => {
                 </ErrorBoundary>
             </Suspense>
         </HydrationBoundary>        
+        </>
     )
 };
  
