07815a2c2cf8b63942c97e56a0467bff6a7ebf0b
yacinesekhri <136460089+yacinesekhri@users.noreply.github.com>
2025-08-26 22:48:50 +0100

    14: agent page

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -7,8 +7,10 @@ import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state copy";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
+import { useRouter } from "next/navigation";
 
 export const AgentsView = () => {
+    const router = useRouter();
     const [filters, setFilters] = useAgentsFilters();
 
     const trpc = useTRPC();
@@ -18,7 +20,7 @@ export const AgentsView = () => {
 
     return(
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <DataTable data={data.items} columns={columns}/>
+            <DataTable data={data.items} columns={columns} onRowClick={(row) => router.push(`/agents/${row.id}`)}/>
             <DataPagination 
             page={filters.page}
             totalPages={data.totalPages}
