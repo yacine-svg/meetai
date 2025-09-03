5cb410bca7f1b1dcbc96488cfe8b25f6ec769fb5
yacinesekhri <136460089+yacinesekhri@users.noreply.github.com>
2025-09-03 21:50:50 +0100

    40

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -21,10 +21,6 @@ export const auth = betterAuth({
             ]
         }),
     ],
-    trustedOrigins: [
-    process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
-    ...(process.env.NODE_ENV !== "production" && process.env.NGROK_URL ? [process.env.NGROK_URL] : []),
-    ],
     socialProviders: {
         google: { 
             clientId: process.env.GOOGLE_CLIENT_ID as string, 
